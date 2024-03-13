9bfd7341f764997ea7fce0b19cecba5d30496bed
Abhishek AN <darkabhi1520@gmail.com>
2024-03-13 23:15:15 +0530

    Integrate Django unfold admin

--- apps/me/admin.py
+++ apps/me/admin.py
@@ -1,11 +1,40 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
 from .models import GymVisit, Meditation, Investment, Sport, Transaction, Vehicle, PhysiqueDetail
 
 # Register your models here.
-admin.site.register(GymVisit)
-admin.site.register(Meditation)
-admin.site.register(Investment)
-admin.site.register(Sport)
-admin.site.register(Transaction)
-admin.site.register(Vehicle)
-admin.site.register(PhysiqueDetail)
+
+
+@admin.register(GymVisit)
+class GymVisitAdminClass(ModelAdmin):
+    pass
+
+
+@admin.register(Meditation)
+class MeditationAdminClass(ModelAdmin):
+    pass
+
+
+@admin.register(Investment)
+class InvestmentAdminClass(ModelAdmin):
+    pass
+
+
+@admin.register(Sport)
+class SportAdminClass(ModelAdmin):
+    pass
+
+
+@admin.register(Transaction)
+class TransactionAdminClass(ModelAdmin):
+    pass
+
+
+@admin.register(Vehicle)
+class VehicleAdminClass(ModelAdmin):
+    pass
+
+
+@admin.register(PhysiqueDetail)
+class PhysiqueDetailAdminClass(ModelAdmin):
+    pass
--- life/settings.py
+++ life/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(' ')
 # Application definition
 
 INSTALLED_APPS = [
+    "unfold",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
